d252d42bed1d72b2e2408206ecb5117476610fb0
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-08-03 01:23:45 +0530

    Posting data to database from signup form

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,50 +4,50 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
     return (
         <div>
-            <nav class="bg-white border-gray-200 py-2.5">
-                <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-                    <Link to="/" class="flex items-center">
-                        <span class="self-center text-xl font-extrabold whitespace-nowrap text-[#667eea]">NimbusBook</span>
+            <nav className="bg-white border-gray-200 py-2.5">
+                <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
+                    <Link to="/" className="flex items-center">
+                        <span className="self-center text-xl font-extrabold whitespace-nowrap text-[#667eea]">NimbusBook</span>
                     </Link>
-                    <div class="flex items-center lg:order-2">
-                        <div class="hidden mt-2 mr-4 sm:inline-block">
+                    <div className="flex items-center lg:order-2">
+                        <div className="hidden mt-2 mr-4 sm:inline-block">
                             <span></span>
                         </div>
                         <Link to="/signup"
-                            class="text-white bg-[#667eea] hover:bg-[#5164bb] focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 focus:outline-none">Signup</Link>
+                            className="text-white bg-[#667eea] hover:bg-[#5164bb] focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 focus:outline-none">Signup</Link>
                         <button data-collapse-toggle="mobile-menu-2" type="button"
-                            class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
                             aria-controls="mobile-menu-2" aria-expanded="true">
-                            <span class="sr-only">Open main menu</span>
-                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
+                            <span className="sr-only">Open main menu</span>
+                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd"
                                     d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                    clip-rule="evenodd"></path>
+                                    clipRule="evenodd"></path>
                             </svg>
-                            <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
+                            <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd"
                                     d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
+                                    clipRule="evenodd"></path>
                             </svg>
                         </button>
                     </div>
-                    <div class="items-center justify-between w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                        <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                    <div className="items-center justify-between w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <Link to="/"
-                                    class="block text-center py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#667eea] lg:p-0">Home</Link>
+                                    className="block text-center py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#667eea] lg:p-0">Home</Link>
                             </li>
                             <li>
                                 <Link to="/about"
-                                    class="block text-center py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#667eea] lg:p-0">About</Link>
+                                    className="block text-center py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#667eea] lg:p-0">About</Link>
                             </li>
                             <li>
                                 <Link to="/features"
-                                    class="block text-center py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#667eea] lg:p-0">Features</Link>
+                                    className="block text-center py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#667eea] lg:p-0">Features</Link>
                             </li>
                             <li>
                                 <Link to="/contact"
-                                    class="block text-center py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#667eea] lg:p-0 ">Contact</Link>
+                                    className="block text-center py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#667eea] lg:p-0 ">Contact</Link>
                             </li>
                         </ul>
                     </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,24 +1,58 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
+
+    document.title = "NimbusBook - Signup";
+
+    const [user, setUser] = useState({
+        fullname: "",
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+        let name = e.target.name;
+        let value = e.target.value;
+
+        setUser({
+            ...user,
+            [name]: value
+        });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:3000/api/v1/auth/register', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(user),
+            });
+        } catch (error) {
+            console.log("Signup failed!!");
+        }
+    }
+
     return (
-        <div class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
-            <div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-                <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+        <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+            <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+                <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
                     <div>
                         <h1 className='text-[#667eea] font-bold text-2xl text-center'>NimbusBook</h1>
                     </div>
-                    <div class="mt-12 flex flex-col items-center">
-                        <h1 class="text-2xl xl:text-3xl font-bold">
+                    <div className="mt-12 flex flex-col items-center">
+                        <h1 className="text-2xl xl:text-3xl font-bold">
                             Create an account
                         </h1>
-                        <div class="w-full flex-1 mt-8">
-                            <div class="flex flex-col items-center">
+                        <div className="w-full flex-1 mt-8">
+                            <div className="flex flex-col items-center">
                                 <button
-                                    class="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
-                                    <div class="bg-white p-2 rounded-full">
-                                        <svg class="w-4" viewBox="0 0 533.5 544.3">
+                                    className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
+                                    <div className="bg-white p-2 rounded-full">
+                                        <svg className="w-4" viewBox="0 0 533.5 544.3">
                                             <path
                                                 d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
                                                 fill="#4285f4" />
@@ -33,60 +67,67 @@ const Signup = () => {
                                                 fill="#ea4335" />
                                         </svg>
                                     </div>
-                                    <span class="ml-4">
+                                    <span className="ml-4">
                                         Sign Up with Google
                                     </span>
                                 </button>
 
                                 <button
-                                    class="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5">
-                                    <div class="bg-white p-1 rounded-full">
-                                        <svg class="w-6" viewBox="0 0 32 32">
-                                            <path fill-rule="evenodd"
+                                    className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5">
+                                    <div className="bg-white p-1 rounded-full">
+                                        <svg className="w-6" viewBox="0 0 32 32">
+                                            <path fillRule="evenodd"
                                                 d="M16 4C9.371 4 4 9.371 4 16c0 5.3 3.438 9.8 8.207 11.387.602.11.82-.258.82-.578 0-.286-.011-1.04-.015-2.04-3.34.723-4.043-1.609-4.043-1.609-.547-1.387-1.332-1.758-1.332-1.758-1.09-.742.082-.726.082-.726 1.203.086 1.836 1.234 1.836 1.234 1.07 1.836 2.808 1.305 3.492 1 .11-.777.422-1.305.762-1.605-2.664-.301-5.465-1.332-5.465-5.93 0-1.313.469-2.383 1.234-3.223-.121-.3-.535-1.523.117-3.175 0 0 1.008-.32 3.301 1.23A11.487 11.487 0 0116 9.805c1.02.004 2.047.136 3.004.402 2.293-1.55 3.297-1.23 3.297-1.23.656 1.652.246 2.875.12 3.175.77.84 1.231 1.91 1.231 3.223 0 4.61-2.804 5.621-5.476 5.922.43.367.812 1.101.812 2.219 0 1.605-.011 2.898-.011 3.293 0 .32.214.695.824.578C24.566 25.797 28 21.3 28 16c0-6.629-5.371-12-12-12z" />
                                         </svg>
                                     </div>
-                                    <span class="ml-4">
+                                    <span className="ml-4">
                                         Sign Up with GitHub
                                     </span>
                                 </button>
                             </div>
 
-                            <div class="my-12 border-b text-center">
+                            <div className="my-12 border-b text-center">
                                 <div
-                                    class="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                                    className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
                                     Or signup with e-mail
                                 </div>
                             </div>
 
-                            <div class="mx-auto max-w-xs">
+                            <form onSubmit={handleSubmit} className="mx-auto max-w-xs">
                                 <input
-                                    class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                                    type="email" placeholder="Full name" />
+                                    name='fullname'
+                                    onChange={handleChange} value={user.fullname}
+                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                                    type="text" placeholder="Full name" />
                                 <input
-                                    class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                                    name='email'
+                                    onChange={handleChange} value={user.email}
+                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
                                     type="email" placeholder="Email" />
                                 <input
-                                    class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                                    name='password'
+                                    onChange={handleChange} value={user.password}
+                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
                                     type="password" placeholder="Password" />
                                 <button
-                                    class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                                    <span class="ml-3">
+                                    type='submit'
+                                    className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                                    <span className="ml-3">
                                         Create an account
                                     </span>
                                 </button>
-                                <p class="mt-6 text-gray-600 text-center text-sm">
+                                <p className="mt-6 text-gray-600 text-center text-sm">
                                     Already have an account?&#160;
-                                    <Link to='/login' class="border-b text-sm border-gray-500 border-dotted">
+                                    <Link to='/login' className="border-b text-sm border-gray-500 border-dotted">
                                         Login here
                                     </Link>
                                 </p>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
-                <div class="flex-1 bg-indigo-100 text-center hidden lg:flex">
-                    <div class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+                <div className="flex-1 bg-indigo-100 text-center hidden lg:flex">
+                    <div className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
                         style={{ backgroundImage: "url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg')" }}>
                     </div>
                 </div>
